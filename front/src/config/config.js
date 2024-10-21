1654e7fc791141abc285cb1eb2526e3e3582c0dd
devkledisom <dev@ziiz.com.br>
2024-10-20 22:35:38 -0300

    building

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,6 +1,6 @@
 export const masterPath = {
-    url: "http://localhost:3032/api"
-    //url: "https://minisitio.online/api"
+    //url: "http://localhost:3032/api"
+    url: "https://minisitio.online/api"
 };
 
 export const version = {
