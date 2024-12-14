70ebc833250838b15d814e3ff09319694cc162f0
devkledisom <dev@ziiz.com.br>
2024-12-14 12:02:14 -0300

    homolog import

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,7 +1,7 @@
 export const masterPath = {
     //url: "http://localhost:3032/api" //LOCAL
-    //url: "https://minisitio.online/api" //HOMOLOGAÇÃO
-    url: "https://br.minisitio.net/api" //PRODUÇÃO
+    url: "https://minisitio.online/api" //HOMOLOGAÇÃO
+    //url: "https://br.minisitio.net/api" //PRODUÇÃO
 };
 
 export const version = {
