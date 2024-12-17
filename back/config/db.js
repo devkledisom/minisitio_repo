529c22079d12d2e7394bf2ec7c709dcbb7cb4790
devkledisom <dev@ziiz.com.br>
2024-12-17 00:08:34 -0300

    correcao modulo gerenciarids homolog2

--- back/config/db.js
+++ back/config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 //PROD
-/* const sequelize = new Sequelize('miniv2_minisitio_prod', 'miniv2_minisitio_dba', 'Para@2023', {
+const sequelize = new Sequelize('miniv2_minisitio_prod', 'miniv2_minisitio_dba', 'Para@2023', {
     dialect: 'mysql',
     host: '51.222.94.129',
     port: 3306,
@@ -12,15 +12,15 @@ const Sequelize = require('sequelize');
         acquire: 30000, // Tempo máximo para adquirir conexão (ms)
         idle: 10000, // Tempo de ociosidade antes de liberar a conexão (ms)
       },
-});   */
+});   
  
 //HOMOLOG
- const sequelize = new Sequelize('minisitio_painel', 'minisitio_teste', 'Para@2023', {
+/*  const sequelize = new Sequelize('minisitio_painel', 'minisitio_teste', 'Para@2023', {
     dialect: 'mysql',
     host: '51.222.94.129',
     port: 3306,
     timezone: '-03:00', // Ajuste conforme o fuso horário necessário
-});   
+});    */
 
 //LOCAL
 /*   const sequelize = new Sequelize('minisitio_teste', 'root', 'root', {
