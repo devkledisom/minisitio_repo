28822cfdde5d306012b2e447b2cd0e286a4094ee
devkledisom <dev@ziiz.com.br>
2025-01-15 12:52:53 -0300

    fix: config.js

--- back/config/config.js
+++ back/config/config.js
@@ -2,5 +2,6 @@ module.exports = {
     //url: "http://localhost:3032/api",
     url: "https://minisitio.online/api",
     //url: "https://br.minisitio.net/api", //PRODUÇÃO
-    domain: "https://minisitio.online"
+    //domain: "https://minitest.minisitio.online",
+    domain: 'https://br.minisitio.net'
 }
