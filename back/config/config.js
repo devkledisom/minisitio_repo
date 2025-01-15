af41538c00a3fb7ad3112c85132e8bf4005ff14a
devkledisom <dev@ziiz.com.br>
2025-01-15 13:20:20 -0300

    fix: config.js prod

--- back/config/config.js
+++ back/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     //url: "http://localhost:3032/api",
-    url: "https://minisitio.online/api",
-    //url: "https://br.minisitio.net/api", //PRODUÇÃO
+    //url: "https://minisitio.online/api",
+    url: "https://br.minisitio.net/api", //PRODUÇÃO
     //domain: "https://minitest.minisitio.online",
     domain: 'https://br.minisitio.net'
 }
--- front/src/config/config.js
+++ front/src/config/config.js
@@ -1,8 +1,9 @@
 export const masterPath = {
     //url: "http://localhost:3032/api", //LOCAL
-    url: "https://minisitio.online/api", //HOMOLOGAÇÃO
-    //url: "https://br.minisitio.net/api", //PRODUÇÃO
-    domain: 'https://minitest.minisitio.online'
+    //url: "https://minisitio.online/api", //HOMOLOGAÇÃO
+    url: "https://br.minisitio.net/api", //PRODUÇÃO
+    //domain: 'https://minitest.minisitio.online'
+    domain: 'https://br.minisitio.net'
 };
 
 export const version = {
