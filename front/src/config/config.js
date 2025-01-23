a286bc7df0e6aadc48741fc0a7def655e806a9b4
devkledisom <dev@ziiz.com.br>
2025-01-23 00:59:23 -0300

    fix: modulo espacos prod

--- front/src/config/config.js
+++ front/src/config/config.js
@@ -14,10 +14,10 @@ export const masterPath = {
 
 export const masterPath = {
     //url: "http://localhost:3032/api", //LOCAL
-    url: "https://minisitio.online/api", //HOMOLOGAÇÃO
-    //url: "https://br.minisitio.net/api", //PRODUÇÃO
-    domain: 'https://minitest.minisitio.online',
-    //domain: 'https://br.minisitio.net',
+    //url: "https://minisitio.online/api", //HOMOLOGAÇÃO
+    url: "https://br.minisitio.net/api", //PRODUÇÃO
+    //domain: 'https://minitest.minisitio.online',
+    domain: 'https://br.minisitio.net',
     accessToken: sessionStorage.getItem('userTokenAccess')
 }; 
 
