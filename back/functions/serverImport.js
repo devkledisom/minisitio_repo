04c38c7291d53b025422bd6ea361ff97e45f2cf2
devkledisom <dev@ziiz.com.br>
2025-06-12 21:54:17 -0300

    fix: 2.0.69 - correção ordem alfa atividade

--- back/functions/serverImport.js
+++ back/functions/serverImport.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 module.exports = function saveImport() {
     // Configura o destino e o nome do arquivo
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/import/'); // Pasta onde o arquivo será salvo
-    },
-    filename: function (req, file, cb) {
-        cb(null, 'uploadedfile.xlsx'/*file.fieldname  + '-' + Date.now() + path.extname(file.originalname) */);
-    }
-});
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'public/import/'); // Pasta onde o arquivo será salvo
+        },
+        filename: function (req, file, cb) {
+            cb(null, 'uploadedfile.csv'/*file.fieldname  + '-' + Date.now() + path.extname(file.originalname) */);
+        }
+    });
 
-const upload = multer({ storage: storage });
+    const upload = multer({ storage: storage });
 
-return upload;
+    return upload;
 }
