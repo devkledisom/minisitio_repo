a98f75b5ef79230bb45e7e3029c350dc4371ecb5
devkledisom <dev@ziiz.com.br>
2025-08-04 15:20:24 -0300

    feat: correcao exportacao atividade

--- back/config/config.js
+++ back/config/config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     //url: "http://localhost:3032/api",
-    //url: "https://minisitio.online/api",
-    url: "https://minisitio.net/api", //PRODUÇÃO
+    url: "https://minitest.automaplay.com.br/api",
+    //url: "https://minisitio.net/api", //PRODUÇÃO
     //domain: "https://minitest.minisitio.online",
     domain: 'https://minisitio.com.br',
     apiSecret: "@rfxzsklc_s+bg7t+@f6^obve=f!swr1%0838lctalor92vi",
--- front/src/components/WebcardThumb.jsx
+++ front/src/components/WebcardThumb.jsx
@@ -152,7 +152,7 @@ function WebcardThumb(props) {
                     <div className='row p-2'>
 
                         {/*  <img src={`${masterPath.url}/files/${props.codImg}`} alt="" width={150} height={200} /> */}
-                        {thumb(imgDefault) && <img src={`${masterPath.url}/${imgDefault}`} alt="" width={150} style={{height: "300px"}} />}
+                        {thumb(imgDefault) && <img src={`${masterPath.url}/${imgDefault}`} alt="" width={150} style={{ height: "300px" }} />}
                     </div>
 
                     {!thumb(imgDefault) &&
@@ -194,15 +194,24 @@ function WebcardThumb(props) {
                                 <div className="col-md-6 col-6">
                                     <p className='text-end'>
                                         Desde: {formatData(props.data.createdAt)}<br />
-                                        {props.data.dtCadastro2 &&
+                                        {props.data.dtCadastro2 ?
                                             <span>
                                                 Renovado em: {formatData(props.data.dtCadastro2)}<br />
                                             </span>
+                                            :
+                                            <span>
+                                                Renovado em: <br />
+                                            </span>
                                         }
-                                        {props.data.dtCadastro2 &&
+                                        {props.data.dtCadastro2 ?
                                             <span>
                                                 Até: {formatData(props.data.dueDate)}
                                             </span>
+                                            :
+                                            <span>
+                                                Até: {formatData(props.data.dueDate)}
+                                            </span>
+
                                         }
                                     </p>
                                 </div>
