bc80654a6e7fd81edb2b69a6a790311d3874c56b
devkledisom <dev@ziiz.com.br>
2025-11-18 18:18:27 -0300

    fix: 2.0.88 - correcao

--- front/build/static/js/851.63c840eb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkminisitio=self.webpackChunkminisitio||[]).push([[851],{7851:(a,e,t)=>{t.r(e),t.d(e,{ShareWeb:()=>r});var i=t(5403);class r extends i.E_{async canShare(){return"undefined"!==typeof navigator&&navigator.share?{value:!0}:{value:!1}}async share(a){if("undefined"===typeof navigator||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}}}}]);
-//# sourceMappingURL=851.63c840eb.chunk.js.map
